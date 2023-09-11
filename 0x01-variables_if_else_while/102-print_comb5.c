0da6d01eb90d4a668dc3a9ff775aca09b360e44f
AbasBoomtown <me.boomtown@gmail.com>
2023-09-11 08:52:50 -0700

    low level variables

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,17 +6,26 @@
  */
 int main(void)
 {
-	int n;
+	int num1, num2;
 
-	for (n = 48; n < 58; n++)
+	for (num1 = 0; num1 <= 99; num1++)
 	{
-		putchar(n);
-		if (n != 57)
+		for (num2 = num1 + 1; num2 <= 99; num2++)
 		{
+			putchar((num1 / 10) + '0');
+			putchar((num1 % 10) + '0');
+			putchar(' ');
+			putchar((num2 / 10) + '0');
+			putchar((num2 % 10) + '0');
+
+			if (num1 == 98 && num2 == 99)
+				continue;
+
 			putchar(',');
 			putchar(' ');
 		}
 	}
+
 	putchar('\n');
 	return (0);
 }
