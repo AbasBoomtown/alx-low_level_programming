5a06532be90851c68bc7f27c2fd3c8fc2ce8517c
AbasBoomtown <me.boomtown@gmail.com>
2023-09-12 17:53:30 -0700

    nested loops

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,15 +4,11 @@
  * print_alphabet - Make the alphabet
  * Return: void
  */
-
 void print_alphabet(void)
-
 {
-	char (c);
+	char c;
 
 	for (c = 'a'; c <= 'z'; c++)
-	{
-	putchar(c);
-	}
- 	putchar('\n');
+_putchar(c);
+_putchar('\n');
 }
