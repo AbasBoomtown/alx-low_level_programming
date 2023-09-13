d496cfb8319960ea8728c0e8a2bb91ac1659260a
AbasBoomtown <me.boomtown@gmail.com>
2023-09-13 12:13:47 -0700

    nested loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - function that adds two integers and returns the result
+ * @n1: the first num to be computed
+ * @n2: the second num to be computed
+ * Return: Integer value
+ */
+
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
