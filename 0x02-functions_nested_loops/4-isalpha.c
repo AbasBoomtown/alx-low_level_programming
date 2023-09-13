2ec4fb79a9a384a3e03d36738f95270069383081
AbasBoomtown <me.boomtown@gmail.com>
2023-09-13 16:20:58 -0700

    nested loops

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,11 +3,13 @@
 /**
  * _isalpha - Writes a function that checks for alphabetic character.
  * @c: the character to be checked
- * Return: 1 if c is a letter, 0 otherwise.
+ * Return: 1 for alphabetic character or 0 for anything else
  */
 int _isalpha(int c)
 {
-	if ((c >= 65 && c <= 90) || c >= 97 || c <= 122)
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+	{
 	return (1);
+	}
 	return (0);
 }
