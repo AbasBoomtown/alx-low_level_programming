0a444ae26e26b84ed08bfd55d468f10c7a09e108
AbasBoomtown <me.boomtown@gmail.com>
2023-09-18 12:55:46 -0700

    pointers and arrays

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -6,12 +6,12 @@
  * @b: integer to swap
  */
 
-void swap_int(int *a, int *b);
+void swap_int(int *a, int *b)
 
 {
 	int m;
 
 	m = *a;
 	*a = *b;
-	*b = m
+	*b = m;
 }
--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -5,7 +5,7 @@
  * @s: string
  * Return: 0
  */
-void print_rev(char *s);
+void print_rev(char *s)
 {
 	int longi = 0
 
