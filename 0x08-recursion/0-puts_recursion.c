e77314dade67c94164151efdf0b0f22d882ec75b
AbasBoomtown <me.boomtown@gmail.com>
2023-09-28 16:20:22 -0700

    recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,18 +1,17 @@
 #include "main.h"
-
 /**
- * _puts_recursion -  function that prints a string,
- * followed by a new line
+ * _puts_recursion - function like puts();
  * @s: input
  * Return: Always 0 (Success)
  */
-Void _puts_recursion(char *s)
+void _puts_recursion(char *s)
 {
 	if (*s)
 	{
 		_putchar(*s);
-		put_recursion(s + 1);
+		_puts_recursion(s + 1);
 	}
+
 	else
 		_putchar('\n');
 }
--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,9 +1,10 @@
 #include "main.h"
- /**
-  * _strlen_recursion -  returns the length of a string
-  * @s: the string to be measured
-  * Return: the length to print
-  */
+/**
+ * _strlen_recursion - Returns the length of a string.
+ * @s: The string to be measured.
+ *
+ * Return: The length of the string.
+ */
 int _strlen_recursion(char *s)
 {
 	int longit = 0;
@@ -11,7 +12,8 @@ int _strlen_recursion(char *s)
 	if (*s)
 	{
 		longit++;
-		longit += strlen_recursion(s + 1);
+		longit += _strlen_recursion(s + 1);
 	}
-	return(longit);
+
+	return (longit);
 }
