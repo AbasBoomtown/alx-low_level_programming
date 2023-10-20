613897b4578cb7d23c5a57dce847720c2bf796c4
AbasBoomtown <me.boomtown@gmail.com>
2023-10-19 21:57:30 -0700

    singly list

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,10 +3,11 @@
 void first(void) __attribute__ ((constructor));
 
 /**
- * first - function prints a sentence before main function is executed
+ * first - prints a sentence before the main
  * function is executed
  */
 void first(void)
+
 {
  printf("You're beat! and yet, you must allow,\n");
  printf("I bore my house upon my back!\n");
